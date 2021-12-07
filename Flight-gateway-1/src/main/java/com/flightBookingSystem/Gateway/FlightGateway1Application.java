a0f06ad06dd2b2f937aa1357fcc4bc9d7369d3b4
gouravpawar <gouravrpawar@gmail.com>
2021-12-07 11:22:06 +0530

    Api Gateway

---
+++ Flight-gateway-1/src/main/java/com/flightBookingSystem/Gateway/FlightGateway1Application.java
@@ -0,0 +1,15 @@
+package com.flightBookingSystem.Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class FlightGateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightGateway1Application.class, args);
+	}
+
+}
---
+++ Flight-gateway-1/src/test/java/com/flightBookingSystem/Gateway/FlightGateway1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightBookingSystem.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightGateway1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
