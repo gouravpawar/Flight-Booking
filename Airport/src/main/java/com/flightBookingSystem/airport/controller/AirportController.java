340356b1545afcd46d95f6a0f0e116b5e2ba75a1
gouravpawar <93312979+gouravpawar@users.noreply.github.com>
2021-12-07 20:06:30 +0530

    Add files via upload

--- Airport/src/main/java/com/flightBookingSystem/airport/controller/AirportController.java
+++ Airport/src/main/java/com/flightBookingSystem/airport/controller/AirportController.java
@@ -1,16 +1,40 @@
 package com.flightBookingSystem.airport.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.flightBookingSystem.airport.models.Airport;
+import com.flightBookingSystem.airport.service.AirportService;
+
 
 @RestController
 @RequestMapping("/airport")
 public class AirportController {
 	
-	@GetMapping("/hello")
 	
-	public Object hello() {
-		return "Hello World For Airportt";
+	@Autowired
+	private AirportService airportService;
+	
+	@RequestMapping("/service")
+	
+	public List<Airport> getAirport() {
+		return airportService.getAllAirports();
+				
 	}
+	
+	  @RequestMapping(method=RequestMethod.POST, value="/service") 
+	
+	  public void addAirport(@RequestBody Airport airport ) {
+	  airportService.addAirport(airport);
+	  }
+	 
+		
+	
+	
 }
